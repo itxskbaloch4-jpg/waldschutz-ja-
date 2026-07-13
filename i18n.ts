80396d6bb1f1f035539c62f71d96d6b7062983f9
Sk Baloch <itxskbaloch4@gmail.com>
2026-07-13 14:45:28 +0200

    Create i18n.ts

---
+++ i18n.ts
@@ -0,0 +1,8 @@
+import { getRequestConfig } from "next-intl/server";
+
+export const locales = ["de", "en", "fr"] as const;
+export const defaultLocale = "de";
+
+export default getRequestConfig(async ({ locale }) => ({
+  messages: (await import(`./messages/${locale}.json`)).default,
+}));
